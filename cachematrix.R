c6d1c522b31282ca6a3450bebffe3bd63a35b125
Justin DeShazor <rjdeshazor@users.noreply.github.com>
2016-09-07 15:36:10 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This series of functions creates a "matrix" object (really a list), 
+## calculates its inverse,and then caches the result, so that the inverse 
+## does not have to be computed repeatedly.
 
-## Write a short comment describing this function
+
+# makeCacheMatrix "creates" a matrix that can cache its inverse by
+# creating a list containing functions that:
+# 1) Set an invertible matrix
+# 2) Get an invertible matrix
+# 3) Set the matrix inverse
+# 4) Get the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  IV <- NULL
+  setM <- function(y){
+    x <<- y
+    IV <<- NULL
+  }
+  getM <- function() x
+  setIV <- function(inverse) IV <<- inverse
+  getIV <- function() IV
+  list(setM=setM, getM=getM, setIV=setIV, getIV=getIV)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve calculates the inverse of the matrices created above:
+# Contains functions that
+# 1) Check to see if the inverse of a matrix has already been calculated. Skips computation and 
+# retrieves from cache if so
+# 2) Calculates inverse of matrices returned by makeCacheMatrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+  IV <- x$getIV()
+  if(!is.null(IV)){
+    message("Getting cached data")
+    return(IV)
+  }
+  IVdata <- x$getM()
+  IV <- solve(IVdata, ...)
+  x$setIV(IV)
+  IV
 }
